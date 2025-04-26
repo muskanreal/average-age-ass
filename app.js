074d259ee4b3a62b2f812462ca80f5d4125c3069
muskanreal <muskanmalik01062006@gmail.com>
2025-04-26 04:14:56 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+let age1 = parseInt(prompt("enter age of student 1"));
+let age2 = parseInt(prompt("enter age of student 2"));
+let age3 = parseInt(prompt("enter age of student 3"));
+let age4 = parseInt(prompt("enter age of student 4"));
+let age5 = parseInt(prompt("enter age of student 5"));
+let age6 = parseInt(prompt("enter age of student 6"));
+let age7 = parseInt(prompt("enter age of student 7"));
+let age8 = parseInt(prompt("enter age of student 8"));
+let age9 = parseInt(prompt("enter age of student 9"));
+let age10 = parseInt(prompt("enter age of student 10"));
+
+let totalAge = age1+ age2+ age3+ age4+ age5+ age6+ age7+ age8+ age9+ age10;
+let averageAge = totalAge/10;
+ console.log(`The average Age is: ${averageAge}`);
